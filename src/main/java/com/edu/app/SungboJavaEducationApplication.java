db7b2094b3ac08f620242435854ccf601cf75f30
sungbo <ishimsungbo@gmail.com>
2022-05-30 09:31:18 +0900

    Initial commit

---
+++ src/main/java/com/edu/app/SungboJavaEducationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungboJavaEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SungboJavaEducationApplication.class, args);
+    }
+
+}
